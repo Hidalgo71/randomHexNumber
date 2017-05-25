0e5971ecf771ef69b217aafc7de4b3814c869250
Yekta Özdemir <lakers_yekta_ceza@hotmail.com>
2017-05-26 01:39:45 +0300

    updated

--- randomHexNumber/Source.c
+++ randomHexNumber/Source.c
@@ -12,5 +12,16 @@ int main()
 
 void randomHex()
 {
+	int hexLen = 0, i, hexNum = 0;
+	int letterArray[16] = { 'A','B','C','D','E','F',0,1,2,3,4,5,6,7,8,9 };
+	
+	printf("Enter The Hex Number Length:");
+	scanf_s("%d", &hexLen);
+
+	for (i = 0; i < hexLen; i++)
+	{
+		hexNum = rand() % 10;
+		printf("%d", hexNum);
+	}
 	
 }
